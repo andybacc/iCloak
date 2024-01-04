28eab12c19f35d799320c6a2ca947ed772a00292
andybacc <andreabacciolo80@gmail.com>
2024-01-04 02:24:41 +0100

    mix

--- src/components/Matrici.jsx
+++ src/components/Matrici.jsx
@@ -1,4 +1,4 @@
-import { Button, FormControl, FormLabel, Input, VStack, useToast } from '@chakra-ui/react'
+import { Button, FormControl, FormLabel, Input, HStack, VStack, useToast } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import apiClient from '../apiClient'
 import useStore from '../store'
@@ -6,19 +6,21 @@ import useStore from '../store'
 const Matrici = () => {
     const [inizio, setInizio] = useState(0)
     const [fine, setFine] = useState(0)
+    const [lable, setLable] = useState('')
     const [isLoading, setIsLoading] = useState(false)
     const toast = useToast()
     const { settings } = useStore()
 
     function stampaMatrici() {
         setIsLoading(true)
-        apiClient.post(`/matrici/${inizio}/${fine}`, {stampanti: settings?.stampanti})
+        apiClient.post(`/matrici/${inizio}/${fine}?lable=${lable}`, {stampanti: settings?.stampanti})
         .then((r) => {
             toast({ title: 'Matrici stampate', status: 'success', isClosable: true })
             setTimeout(() => {
                 setIsLoading(false)
                 setFine(0)
                 setInizio(0)
+                setLable('')
             }, 1000);
         })
         .catch((e) => {
@@ -32,22 +34,30 @@ const Matrici = () => {
     <>
     <FormControl id="matrice">
         <FormLabel>Stampa matrici</FormLabel>
-        <VStack pb='4'>
+        <HStack pb='4'>
+        <Input 
+            type="text" 
+            value={lable}
+            onChange={(e) => setLable(e.target.value)}
+            placeholder="Etichetta"
+        />
         <Input 
             type="number" 
             inputMode="numeric"
             value={inizio>0? inizio : ''}
             onChange={(e) => setInizio(e.target.value)}
-            placeholder="Numero iniziale"
+            placeholder="Inizio"
+            width='120px'
         />
         <Input 
             type="number" 
             inputMode="numeric"
             value={fine>0? fine : ''}
             onChange={(e) => setFine(e.target.value)}
-            placeholder="Numero finale"
+            placeholder="fine"
+            width='120px'
         />
-        </VStack>
+        </HStack>
         <Button isDisabled={inizio==0||!inizio&&fine==0||!fine||inizio>=fine} isLoading={isLoading} size='lg' onClick={()=>stampaMatrici()} >Stampa</Button>
     </FormControl>
 
